7d16e7c7bdbdf5339124eb1ad72176747428f303
Juliano Antonio de Abreu Pereira <jubamarvim@yahoo.com.br>
2022-12-18 18:12:46 -0300

    Add files via upload
    First commit (add js file)

---
+++ 01_4operacoesJS/app_ex.js
@@ -0,0 +1,26 @@
+let b = 13;
+let a = 45;
+
+function somar_valores ( )
+{
+  console.log( `A soma entre ${ a } e ${ b } é igual a ${ a + b }` );
+}
+somar_valores();
+
+function subtrair_valores ( )
+{
+  console.log( `A diferença entre ${ a } e ${ b } é igual a ${ a - b }` );
+}
+subtrair_valores();
+
+function multiplicar_valores ( )
+{
+  console.log( `A multiplicação entre ${ a } e ${ b } é igual a ${ a * b }` );
+}
+multiplicar_valores();
+
+function dividir_valores ( )
+{
+  console.log( `A divisão entre ${ a } e ${ b } é igual a ${ a / b }` );
+}
+dividir_valores( );
